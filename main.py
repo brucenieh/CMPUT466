7752013ef171a4bc2858ae75fece530f0fa7d79f
Kean Weng Yap <yapkeanweng1223@gmail.com>
2021-02-22 00:08:08 -0700

    ngram model.

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
 import raw.readingfiles as readfiles
-import models.ngrams as ngrams
+from models.ngrams import Ngrams
 
 if __name__ == '__main__':
     # readfiles.make_datasets()
-    ngrams.ngrams()
+    ngrams_model = Ngrams(2)
+    x = [['1','2','3','4','5'], ['4','6','4','6']]
+    ngrams_model.train(x)
