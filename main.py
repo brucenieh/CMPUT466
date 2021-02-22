46e6357143e5f071929b17ae686277bd55f1b50f
Kean Weng Yap <yapkeanweng1223@gmail.com>
2021-02-21 22:53:05 -0700

    Create ngrams file.

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
 import raw.readingfiles as readfiles
+import models.ngrams as ngrams
 
 if __name__ == '__main__':
-    readfiles.make_datasets()
+    # readfiles.make_datasets()
+    ngrams.ngrams()
---
+++ models/ngrams.py
@@ -0,0 +1,2 @@
+def ngrams():
+    print("Hello")
