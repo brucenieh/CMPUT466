51f9567854b8445caf69ad10fecb69f920a33198
Bruce Nie <ynie@ualberta.ca>
2021-03-02 01:39:36 -0700

    Added automation for tweaking hyper parameter n.
    Signed-off-by: Bruce Nie <ynie@ualberta.ca>

--- main.py
+++ main.py
@@ -11,3 +11,14 @@ if __name__ == '__main__':
     # ngrams_model.train(data_set[0].training_set)
     # ngrams_model.train(x)
     print(evaluate(ngrams_model, data_set[0].training_set, data_set[0].validation_set))
+
+    # tweak hyper parameter n of n-gram model
+    best_n = (1,0) #n value, accuracy
+    for n in range(2,8):
+        ngrams_model = Ngrams(n)
+        accuracy = evaluate(ngrams_model, data_set[0].training_set, data_set[0].validation_set)
+        print(n,accuracy)
+        if accuracy > best_n[1]:
+            best_n = (n,accuracy)
+    
+    print(best_n)
