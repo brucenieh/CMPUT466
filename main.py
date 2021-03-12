cb2404f32b81b6ee40aa4829747a3cc9d77efb1f
Bruce Nie <ynie@ualberta.ca>
2021-03-11 22:45:03 -0700

    Added jupyter notebook visualization for parameter tweaking and n-gram now returns both accuracy and perplexity
    Signed-off-by: Bruce Nie <ynie@ualberta.ca>

--- main.py
+++ main.py
@@ -1,18 +1,20 @@
 import raw.readingfiles as readfiles
 from models.ngrams import Ngrams
-from util import evaluate, k_fold
+from util import evaluate, k_fold,read_data
 
 if __name__ == '__main__':
     # readfiles.make_datasets()
     data_set = k_fold(10, 'training_data.csv')
-    testing_set = k_fold(2, 'testing_data.csv')
+    training_set = read_data('training_data.csv')
+    testing_set = read_data('testing_data.csv')
+
 
     # ngrams_model.train(data_set[0].training_set)
     # ngrams_model.train(x)
     # print(evaluate(ngrams_model, data_set[0].training_set, data_set[0].validation_set))
 
     # tweak hyper parameter n of n-gram model
-    best_n = (3,0) #n value, accuracy
+    # best_n = (3,0) #n value, accuracy
     # for n in range(2,8):
     #     ngrams_model = Ngrams(n)
     #     accuracy = evaluate(ngrams_model, data_set[0].training_set, data_set[0].validation_set)
@@ -20,8 +22,6 @@ if __name__ == '__main__':
     #     if accuracy > best_n[1]:
     #         best_n = (n,accuracy)
     
-    print(best_n)
-    ngrams_model = Ngrams(best_n[0])
-    accuracy = evaluate(ngrams_model, data_set[0].training_set + data_set[0].validation_set, 
-                        testing_set[0].training_set + testing_set[0].validation_set)
-    print(accuracy)
+    ngrams_model = Ngrams(3)
+    accuracy,perplexity = evaluate(ngrams_model, training_set, testing_set)
+    print(accuracy,perplexity)
--- util.py
+++ util.py
@@ -10,6 +10,18 @@ class Training_data():
         self.validation_set = None
         self.training_set = None
 
+def read_data(path):
+    """return dataset as a list of
+
+    Args:
+        path (str): path to the data file in csv format
+    """
+    # Read csv file, convert 'text' column from string to list
+    df = pandas.read_csv(path, converters={'text': eval})
+    # Convert the Series object to a python list
+    data = df['text'].tolist()
+    return data
+
 def k_fold(k,path):
     """split dataset into k folds for cross validation
 
@@ -52,14 +64,16 @@ def evaluate(model,training_data,testing_data):
         model.train(training_data)
     except Exception as e:
         print('model training failed\n', e)
+        exit()
     
     for test in testing_data:
-        sentence = test[0:-1]
-        target = test[-1]
+        sentence = test[0:-2]
+        target = test[-2]
         try:
             prediction,prob_distrib = model.predict(sentence)
         except Exception as e:
             print('model predicting failed\n', e)
+            exit()
         
         # Update accuracy
         if prediction[0] == target:
@@ -71,7 +85,8 @@ def evaluate(model,training_data,testing_data):
                 perplexity += 1/prob_distrib[target]
             else:
                 perplexity += 1/prob_distrib['<UNK>']
-        
+        else:
+            perplexity += 1/0.000001
     
     accuracy = accuracy/len(testing_data)
     perplexity = perplexity/len(testing_data)
