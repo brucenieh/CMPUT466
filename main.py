0d9508fddec248fe944319fce7afcd7fb895efaf
KumailHusain <shusain@ualberta.ca>
2021-03-26 16:34:09 +0500

    Revert last 2 commits
    This reverts commits
    4d8b0d1facb77b27ceb5562e2e51f6aad9d85e5c
    18b8ba52db4e8d0b4ba285de544f3dcb83ed8513

--- main.py
+++ main.py
@@ -1,19 +1,13 @@
 import raw.readingfiles as readfiles
 from models.ngrams import Ngrams
 from models.ANN import ANN
-from util import build_vocab, evaluate, k_fold, read_data, evaluate_ANN
-
-def data_reader(data,batch_size):
-    # datas = []
-    # print(len(data)//batch_size)
-    for i in range(0, len(data), batch_size):
-        yield data[i:i + batch_size]
+from util import evaluate, k_fold,read_data
 
 if __name__ == '__main__':
     # readfiles.make_datasets()
     data_set = k_fold(10, 'training_data.csv')
-    # training_set = read_data('training_data.csv')
-    # testing_set = read_data('testing_data.csv')
+    training_set = read_data('training_data.csv')
+    testing_set = read_data('testing_data.csv')
 
 
     # ngrams_model.train(data_set[0].training_set)
@@ -33,33 +27,8 @@ if __name__ == '__main__':
     # accuracy,perplexity = evaluate(ngrams_model, training_set, testing_set)
     # print(accuracy,perplexity)
 
-    # build_vocab(data_set[0].training_set, 'vocab')
-    ANN_model = ANN()
-    
-    
-    print(len(ANN_model.mapping))
-    print("done vocab")
 
-    
-    counter = 1
-    total = int(len(data_set[0].training_set)/1000) + 1
-    for _ in range(ANN_model.epoch):
-        dr = data_reader(data_set[0].training_set, 1000)
-        for data in dr:
-            print(f"Batch {counter} of {total}")
-            counter += 1
-            ANN_model.train(data)
-        # print(data)
-    ANN_model.save()
-    
-    # ANN_model.train(data_set[0].training_set[-1000:])
-    # ANN_model.save()
-    print(evaluate_ANN(ANN_model, data_set[0].training_set[-100:], ANN_model.mapping))
-    # print(ANN_model.mapping['test'])
+    ANN_model = ANN()
     # print(training_set[:5])
-    # build_vocab(data_set[0].training_set, 'vocab')
-    # ANN_model.train(data_set[0].training_set[:5000])
-    # print(data_set[0].validation_set)
-    # for i in range(10):
-    #     ANN_model.predict(data_set[0].validation_set[i][:50])
+    ANN_model.train(training_set[:5000])
     # ANN_model.train(["abc"])
